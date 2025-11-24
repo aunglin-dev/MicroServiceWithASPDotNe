bc15c64ce61faefaf671bea6f9dce00d18456ee1
Aung_Lin <linoscar724@gmail.com>
2025-11-25 03:43:50 +0630

    [feat] : Catalog API product Create , [add]: mediator to separate command (write) and query (read)

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,38 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(string Name, string Description, string ImageFile, List<string> Category, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Create Product Entity from Command Object
+
+        Product product = new Product()
+        {
+            Name = command.Name,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+            Category = command.Category,
+        };
+
+        //TODO
+        //Save to the DB
+
+
+        //Create Product Result
+        return new CreateProductResult(Guid.NewGuid());
+
+       
+    }
+}
+
