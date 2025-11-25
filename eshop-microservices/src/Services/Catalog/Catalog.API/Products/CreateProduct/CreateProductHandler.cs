bb8d0c22d86ace0ad9d2a2b11b5d67949c8d89af
Aung_Lin <linoscar724@gmail.com>
2025-11-26 02:12:20 +0630

    [add]: marten transaction db

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿using System.Diagnostics.Eventing.Reader;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -10,7 +9,8 @@ public record CreateProductCommand(string Name, string Description, string Image
 public record CreateProductResult(Guid Id);
 
 
-internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHandler(IDocumentSession session) : 
+    ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -27,6 +27,8 @@ internal class CreateProductHandler : ICommandHandler<CreateProductCommand, Crea
 
         //TODO
         //Save to the DB
+        session.Store(product);
+        await session.SaveChangesAsync();
 
 
         //Create Product Result
--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -4,6 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
+builder.Services.AddMarten(options => options.Connection(builder.Configuration.GetConnectionString("Database")!))
+.UseLightweightSessions();
+
+
+
+
 var app = builder.Build();
 
 
