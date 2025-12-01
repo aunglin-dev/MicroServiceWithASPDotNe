3045bd6758d817d2508250a5fe19f369e7078942
Aung_Lin <linoscar724@gmail.com>
2025-12-01 23:14:28 +0630

    [feat]: GetProductBy Id Endpoint and Handler

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Excepiton/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Excepiton;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("Specific Product Not Found!") 
+    {
+    }
+}
+
--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProduct/GetProductEndpoint.cs
@@ -1,12 +1,12 @@
-﻿namespace Catalog.API.Products.GetProduct
-{
+﻿namespace Catalog.API.Products.GetProduct;
+
 
     public record GetProductResponse(IEnumerable<Product> Products);
     public class GetProductEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/getProduct",async(ISender sender) =>
+            app.MapGet("/products",async(ISender sender) =>
             {
                 var result = await sender.Send(new GetProductQuery());
                 var response = result.Adapt<GetProductResponse>();
@@ -19,4 +19,4 @@
             .WithDescription("Get Product");
         }
     }
-}
+
