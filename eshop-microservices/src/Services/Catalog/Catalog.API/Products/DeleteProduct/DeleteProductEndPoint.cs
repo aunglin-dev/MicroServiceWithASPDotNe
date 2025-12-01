f5cd4cf990ad4c28a19c7034e9c5811146b1c424
Aung_Lin <linoscar724@gmail.com>
2025-12-02 00:20:14 +0630

    [feat]: Delete Product EndPoint and Handler

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndPoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsDelete);
+
+public class DeleteProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/product/{Id}",async(Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(Id));
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+
+        }).WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+    }
+}
+
