a5834966550bffa157073a71735850100a1d3c3c
Aung_Lin <aungzayarlin@acedatasystems.com>
2026-04-13 23:17:41 +0630

    [Feat]: Add Redis , decorator  by using scurtor library and Redis Docker Setup

---
+++ eshop-microservices/src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache)
+    : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(username, cancellationToken);
+        if (!string.IsNullOrEmpty(cachedBasket))
+            return JsonConvert.DeserializeObject<ShoppingCart>(cachedBasket)!;
+
+        //if cache is empty 
+        var basket = await repository.GetBasket(username, cancellationToken);
+        await cache.SetStringAsync(username, JsonConvert.SerializeObject(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StorBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        await repository.StorBasket(basket, cancellationToken);
+        await cache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(username, cancellationToken);
+        await cache.RemoveAsync(username, cancellationToken);
+        return true;
+    }
+}
--- eshop-microservices/src/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Exceptions.Handler;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,8 +29,26 @@ builder.Services.AddMarten(opts =>
 #region Services Registration
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+
+/*
+builder.Services.AddScoped<IBasketRepository>(provider =>
+{
+    var basketRepository = provider.GetRequiredService<BasketRepository>();
+
+    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+});
+*/
 
 #endregion
+
+//Redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
