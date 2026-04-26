b219cca6aa986d377c5d0f2096e9449d09629baa
Aung_Lin <aungzayarlin@acedatasystems.com>
2026-04-26 23:42:50 +0630

    [Feat]: Code first approach to Discount.GRPC

---
+++ eshop-microservices/src/Services/Disount.Grpc/Data/Extension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Disount.Grpc.Data;
+
+public static class Extension
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbcontext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        dbcontext.Database.MigrateAsync();
+
+        return app;
+    }
+}
