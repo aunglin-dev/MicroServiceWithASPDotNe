406acc2cf9e62a63cdeb19eda8d127d9dabf2bd0
Aung_Lin <aungzayarlin@acedatasystems.com>
2026-04-27 23:58:42 +0630

    [Feat]: CRUD for discount GRPC

--- eshop-microservices/src/Services/Disount.Grpc/Services/DiscountService.cs
+++ eshop-microservices/src/Services/Disount.Grpc/Services/DiscountService.cs
@@ -1,31 +1,73 @@
-﻿using Grpc.Core;
+﻿using Disount.Grpc.Data;
+using Disount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Disount.Grpc.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext _dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        //TODO: Db operation 
-        return base.GetDiscount(request, context);
-    }
-    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
-    {
-        //TODO : Db Operation 
-        return base.CreateDiscount(request, context);
-    }
+        var coupon = await _dbContext.Coupons.AsNoTracking()
+            .FirstOrDefaultAsync(el => el.ProductName == request.ProductName)!;
 
-    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
-    {
-        //TODO : Db Operation 
-        return base.UpdateDiscount(request, context);
+        if (coupon is null)
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+        logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}",
+            coupon.ProductName, coupon.Amount);
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
     }
 
-    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
-        //TODO : Db Operation 
-        return base.DeleteDiscount(request, context);
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request"));
+
+        _dbContext.Coupons.Add(coupon);
+        await _dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is created for ProductName : {productName}", coupon.ProductName);
+
+        return coupon.Adapt<CouponModel>();
     }
 
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
 
-} 
+        _dbContext.Coupons.Update(coupon);
+        await _dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request,
+        ServerCallContext context)
+    {
+        var coupon = await _dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.NotFound,
+                $"Discount with ProductName={request.ProductName} is not found."));
+
+        _dbContext.Coupons.Remove(coupon);
+        await _dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully deleted. ProductName : {ProductName}", request.ProductName);
+
+        return new DeleteDiscountResponse { Success = true };
+    }
+}
